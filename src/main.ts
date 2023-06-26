4dfbff37706bcdc3d57bc568b2b2f2ca1c5e69a2
jereconjota <jeremiasfigueroa69@gmail.com>
2023-06-26 12:39:50 -0300

    Add global prefix, enable CORS, and clarify purpose of validation pipe in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // this is for 
+  app.useGlobalPipes(new ValidationPipe()); // this is for validation of DTOs
+  app.enableCors(); 
+  app.setGlobalPrefix('api'); 
   await app.listen(3000);
 }
 bootstrap();
