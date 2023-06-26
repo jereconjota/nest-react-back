4a2b9f2a8d08b3534c47d1b9e51cb623fcc254f4
jereconjota <jeremiasfigueroa69@gmail.com>
2023-06-26 13:04:05 -0300

    Add @nestjs/config dependency and set up ConfigModule for environment variables, including PORT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     TasksModule,
     MongooseModule.forRoot('mongodb://localhost:27017/tasksdb'),
+    ConfigModule.forRoot()
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe()); // this is for validation of DTOs
   app.enableCors(); 
   app.setGlobalPrefix('api'); 
-  await app.listen(3000);
+
+  const port = app.get(ConfigService).get('PORT');
+  await app.listen(port || 3000 );
 }
 bootstrap();
